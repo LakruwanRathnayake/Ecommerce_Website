a052dd52e2f717728d8bb85ef06991fb09f86ef0
manod123 <rathnayakelakruwan321@gmail.com>
2022-12-01 00:56:22 +0530

    Update

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Products from './Products';
+
+const Home = () => {
+    return (
+        <div className='Hero'>
+            <div className="card text-bg-dark border-0">
+                <img src="assets/bg.jpg" className="card-img" alt="background" height="550px"/>
+                    <div className="card-img-overlay d-flex  ">
+                        <div className="container">
+                        <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+                        <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+                        </div>
+                        
+                        
+                    </div>
+            </div>
+            <Products/>
+        </div>
+    );
+};
+
+export default Home;
